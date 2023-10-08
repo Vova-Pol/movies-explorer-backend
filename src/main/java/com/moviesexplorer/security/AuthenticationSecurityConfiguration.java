a5c0149ae7cbcd2d24460ced92b7240038b2c421
Vova Pol <ya-chai777@yandex.ru>
2023-10-08 13:53:22 +0300

    Добавлен базовый конфиг для Security

---
+++ src/main/java/com/moviesexplorer/security/AuthenticationSecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.moviesexplorer.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthenticationSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+}
