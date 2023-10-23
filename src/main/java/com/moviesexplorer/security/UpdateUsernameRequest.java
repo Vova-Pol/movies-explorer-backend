cb3c1512273187d602613aab8862ab9ba3fe6078
Vova Pol <ya-chai777@yandex.ru>
2023-10-23 15:34:52 +0300

    Создан объект UpdateUsernameRequest для обновления username

---
+++ src/main/java/com/moviesexplorer/security/UpdateUsernameRequest.java
@@ -0,0 +1,27 @@
+package com.moviesexplorer.security;
+
+public class UpdateUsernameRequest {
+    private String username;
+    private String password;
+
+    public UpdateUsernameRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
