da3e5f218d634a2434d83bbcd7a9b1c366403448
Vova Pol <ya-chai777@yandex.ru>
2023-11-07 14:32:04 +0300

    AuthResponse и SignoutResponse перенесены c class на record

--- src/main/java/com/moviesexplorer/security/AuthResponse.java
+++ src/main/java/com/moviesexplorer/security/AuthResponse.java
@@ -1,28 +1,4 @@
 package com.moviesexplorer.security;
 import com.moviesexplorer.user.User;
 
-public class AuthResponse {
-    private User user;
-    private String jwt;
-
-    public AuthResponse(User user, String jwt) {
-        this.user = user;
-        this.jwt = jwt;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getJwt() {
-        return jwt;
-    }
-
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
-    }
-}
+public record AuthResponse (User user, String jwt ) {}
