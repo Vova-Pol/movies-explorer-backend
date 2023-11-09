521c0ae8c4fcf5bd69aa7f9a8e65624372fd38fa
Vova Pol <ya-chai777@yandex.ru>
2023-11-09 15:24:21 +0300

    Добавлены контроллеры для Movie

--- src/main/java/com/moviesexplorer/movie/MovieController.java
+++ src/main/java/com/moviesexplorer/movie/MovieController.java
@@ -1,23 +1,45 @@
 package com.moviesexplorer.movie;
 
+import com.moviesexplorer.exceptions.UserNotFoundException;
 import com.moviesexplorer.jpa.MovieRepository;
+import com.moviesexplorer.jpa.UserRepository;
+import com.moviesexplorer.user.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
     private MovieRepository movieRepository;
+    private UserRepository userRepository;
 
-    public MovieController(MovieRepository movieRepository) {
+    public MovieController(MovieRepository movieRepository, UserRepository userRepository) {
         this.movieRepository = movieRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/movies")
-    public void getAllMovies() {}
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
 
     @PostMapping("/movies")
-    public void postMovie(@RequestBody Movie movie) {}
+    public Movie postMovie(HttpServletRequest request, @RequestBody Movie movie) {
+        String username = (String) request.getAttribute("username");
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UserNotFoundException("User wasn't found")
+        );
+
+        movie.setOwner(user);
+        System.out.println(movie);
+        return movieRepository.save(movie);
+    }
 
     @DeleteMapping("/movie/{id}")
-    public void deleteMovie(@PathVariable Long id) {}
+    public void deleteMovie(@PathVariable Long id) {
+        movieRepository.deleteById(id);
+    }
 
 }
